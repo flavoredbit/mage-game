faad9f74546871cbdf2953205872c1f1df5b1224
Flavored Bit <flavoredbit47@gmail.com>
2025-10-20 21:36:25 +0800

    Basic Sokol setup

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,57 @@
-const std = @import("std");
-const mage_game = @import("mage_game");
+const state = struct {
+    var pass_action: sg.PassAction = .{};
+};
+
+export fn init() void {
+    sg.setup(.{
+        .environment = sglue.environment(),
+        .logger = .{ .func = slog.func },
+    });
+
+    state.pass_action.colors[0] = .{
+        .load_action = .CLEAR,
+        .clear_value = .{ .r = 0.25,  .g = 0.5, .b = 0.75 },
+    };
+}
+
+export fn input(e: ?*const sapp.Event) void {
+    if (e == null) return;
+    const event = e.?;
+    if (event.type == .KEY_DOWN) {
+        switch (event.key_code) {
+            .ESCAPE => sapp.quit(),
+            else => {},
+        }
+    }
+}
+
+export fn frame() void {
+    const dt: f32 = @floatCast(sapp.frameDuration() * 60);
+    _ = dt;
+}
+
+export fn cleanup() void {
+    sg.shutdown();
+}
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try mage_game.bufferedPrint();
+    sapp.run(.{
+        .init_cb = init,
+        .event_cb = input,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .width = 1024,
+        .height = 768,
+        .sample_count = 4,
+        .icon = .{ .sokol_default = true },
+        .window_title = "Mage Game",
+        .logger = .{ .func = slog.func },
+    });
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
+const std = @import("std");
+const sokol = @import("sokol");
+const sapp = sokol.app;
+const sg = sokol.gfx;
+const sglue = sokol.glue;
+const slog = sokol.log;
