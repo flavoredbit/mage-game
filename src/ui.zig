da77822bcff60b2b86c5a8392928ead6f468bef3
Flavored Bit <flavoredbit47@gmail.com>
2025-11-01 10:48:02 +0800

    Start drawing dialog box

--- src/ui.zig
+++ src/ui.zig
@@ -1,17 +1,37 @@
 pub fn drawDialog(char_name: []const u8, dialog: []const u8) void {
     _ = char_name;
     _ = dialog;
-    const start_x = 0.0;
-    const start_y = 7.0;
-    const end_x = 15.0;
-    const end_y = 11.0;
+    const start_x = 0;
+    const start_y = 7;
+    const end_x = 15;
+    const end_y = 11;
 
     // dialog box texture 15-17 on x, 0-2 on y
-    // draw corners
-    renderer.drawTile(.interface, start_x, start_y, 15, 0);
-    renderer.drawTile(.interface, end_x, start_y, 17, 0);
-    renderer.drawTile(.interface, start_x, end_y, 15, 2);
-    renderer.drawTile(.interface, end_x, end_y, 17, 2);
+    renderer.drawTile(.interface, @floatFromInt(start_x), @floatFromInt(start_y), 15, 0);
+    renderer.drawTile(.interface, @floatFromInt(end_x), @floatFromInt(start_y), 17, 0);
+    renderer.drawTile(.interface, @floatFromInt(start_x), @floatFromInt(end_y), 15, 2);
+    renderer.drawTile(.interface, @floatFromInt(end_x), @floatFromInt(end_y), 17, 2);
+
+    const border_x_start = start_x + 1;
+    for (border_x_start..end_x) |border_x| {
+        renderer.drawTile(.interface, @floatFromInt(border_x), start_y, 16, 0);
+        renderer.drawTile(.interface, @floatFromInt(border_x), end_y, 16, 2);
+    }
+    const border_y_start = start_y + 1;
+    for (border_y_start..end_y) |border_y| {
+        renderer.drawTile(.interface, start_x, @floatFromInt(border_y), 15, 1);
+        renderer.drawTile(.interface, end_x, @floatFromInt(border_y), 17, 1);
+    }
+
+    const middle_x_start = border_x_start;
+    const middle_y_start = border_y_start;
+    const middle_x_end = end_x;
+    const middle_y_end = end_y;
+    for (middle_x_start..middle_x_end) |middle_x| {
+        for (middle_y_start..middle_y_end) |middle_y| {
+            renderer.drawTile(.interface, @floatFromInt(middle_x), @floatFromInt(middle_y), 16, 1);
+        }
+    }
 }
 
 const std = @import("std");
